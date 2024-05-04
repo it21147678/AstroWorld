7b0b6d01c00b2c50789c5b365f3646507d6c2b32
Esitha Kavisara Jayawardhana <it21147678@my.sliit.lk>
2024-05-05 03:05:40 +0530

    Update useAuth.js

--- client/src/utils/useAuth.js
+++ client/src/utils/useAuth.js
@@ -1,35 +1,39 @@
 import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation  } from 'react-router-dom';
 import { jwtDecode } from 'jwt-decode'; // Assuming you're using jwt-decode for decoding JWT tokens
 
 const useAuth = () => {
   const navigate = useNavigate();
+  const location = useLocation();
 
   useEffect(() => {
     const token = localStorage.getItem('authToken');
+    const isRegisterRoute = location.pathname === '/register';
 
-    if (!token) {
+    if (!token && !isRegisterRoute) {
       // No token found, redirect to login page
       navigate('/'); // Change this to your login route
       return;
     }
 
-    try {
-      // Verify the token
-      const decodedToken = jwtDecode(token);
-
-      // Check if token is expired
-      const currentTime = Date.now() / 1000;
-      if (decodedToken.exp < currentTime) {
-        localStorage.removeItem('authToken');
-        navigate('/'); // Redirect to login page if token is expired
+    if (token) {
+        try {
+          // Verify the token
+          const decodedToken = jwtDecode(token);
+  
+          // Check if token is expired
+          const currentTime = Date.now() / 1000;
+          if (decodedToken.exp < currentTime) {
+            localStorage.removeItem('authToken');
+            navigate('/'); // Redirect to login page if token is expired
+          }
+        } catch (error) {
+          // Token is invalid, redirect to login page
+          localStorage.removeItem('authToken');
+          navigate('/'); // Redirect to login page
+        }
       }
-    } catch (error) {
-      // Token is invalid, redirect to login page
-      localStorage.removeItem('authToken');
-      navigate('/'); // Redirect to login page
-    }
-  }, [navigate]);
+    }, [navigate, location.pathname]);
 
   return null; // This hook doesn't return any UI elements
 };
